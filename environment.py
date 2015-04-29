f03836f66fe2414b5b7d141a4d755259d0780ed3
Marie-Caroline Finke <marie-caroline.finke@students.olin.edu>
2015-04-29 19:32:05 -0400

    CHanges to environment

--- environment.py
+++ environment.py
@@ -68,6 +68,55 @@ class RainforestTree(Nature):
         if window.model.wet > 0:
             window.model.wet -= 0.05
 
+class DesertPalmTree(Nature):
+    """
+    Desert palm tree object
+    in the future, will make the environment hotter and drier?
+    """
+    def __init__(self, x, y, window):
+        Nature.__init__(self, x, y, window)
+        self.image = pygame.image.load("Images/desertpalmtree.png")
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+    def effect(self, window):
+        if window.model.heat < 255:
+            window.model.heat += 0.01
+        if window.model.wet > 0:
+            window.model.wet -= 0.01
+
+    def negeffect(self, window):
+        if window.model.heat > 0:
+            window.model.heat -= 0.05
+        if window.model.wet < 255:
+            window.model.wet += 0.05
+
+class DesertHill(Nature):
+    """
+    Desert palm tree object
+    in the future, will make the environment hotter and drier?
+    """
+    def __init__(self, x, y, window):
+        Nature.__init__(self, x, y, window)
+        self.image = pygame.image.load("Images/deserthill.png")
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+    def effect(self, window):
+        if window.model.heat < 255:
+            window.model.heat += 0.01
+        if window.model.wet > 0:
+            window.model.wet -= 0.01
+
+    def negeffect(self, window):
+        if window.model.heat > 0:
+            window.model.heat -= 0.05
+        if window.model.wet < 255:
+            window.model.wet += 0.05
+
+
 class Environ(pygame.sprite.Group):
     """
     list of all environment objects
